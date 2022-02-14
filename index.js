f870770cdeb7c5c6b3ad9d045a1a899ecf47c509
Hussein Fawaz <husseinf@onswitchboard.com>
2022-02-13 19:23:58 -0800

    Added error page and function to render error page when viewing rectangles or routes that do not exist

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const { Pool } = require('pg');
 const PORT = process.env.PORT || 5000;
 
 // Database
-const pool = new Pool({ connectionString: process.env.DATABASE_URL }); // fix SSL error?
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+// Express
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -19,105 +19,90 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Routes
-
 app.get('/', (req, res) => {
-  console.log('[GET]:', '/');
-  res.send('Welcome to the server!');
+  res.render('pages/home');
 });
 
 app.get('/rectangles', (req, res) => {
-  console.log('[GET]:', '/rectangles');
-
   const poolQuery = 'SELECT * FROM rectangle';
 
   pool.query(poolQuery, (err, result) => {
     if (err) console.log(err);
-  
-    const rows = result.rows;
-
-    res.render('pages/index', { rows });
+    else res.render('pages/rectangles', { rows: result.rows });
   });
 });
 
 app.post('/rectangles', (req, res) => {
-  console.log('[POST]:', '/rectangles');
-
-  const {
-    name,
-    color,
-    width,
-    height,
-  } = req.body;
-
+  const { name, color, width, height } = req.body;
   const poolQuery = `INSERT INTO rectangle (name, color, width, height) VALUES ('${name}', '${color}', '${width}', '${height}')`;
 
   pool.query(poolQuery, (err, result) => {
     if (err) console.log(err);
-    res.redirect('/rectangles');
+    else res.redirect('/rectangles');
   });
 });
 
-app.get('/rectangles/:id', (req, res) => { // handles reading, updating, and deleting
-  console.log('[GET]:', '/rectangles/:id');
-
+app.get('/rectangles/:id', (req, res) => {
   const id = req.params.id;
 
-  const poolQuery = `SELECT * FROM rectangle WHERE id='${id}'`;
+  if (!isNaN(id)) {
+    const poolQuery = `SELECT * FROM rectangle WHERE id='${id}'`;
 
-  pool.query(poolQuery, (err, result) => {
-    if (err) console.log(err);
-
-    const row = result.rows[0];
-
-    res.render('pages/rectangle', { row });
-  });
+    pool.query(poolQuery, (err, result) => {
+      if (err) console.log(err);
+      else if (result.rows.length === 0) res.render('pages/error');
+      else if (result.rows.length > 0) res.render('pages/rectangle', { row: result.rows[0] });
+    });
+  } else {
+    res.render('pages/error');
+  }
 });
 
 app.post('/rectangles/:id/update', (req, res) => {
-  console.log('[GET]:', '/rectangles/:id/update');
-
   const id = req.params.id;
 
-  const {
-    name,
-    color,
-    width,
-    height,
-  } = req.body;
-
-  const poolQuery = `UPDATE rectangle SET (name, color, width, height) = ('${name}', '${color}', '${width}','${height}') WHERE id='${id}'`;
-
-  pool.query(poolQuery, (err, result) => {
-    if (err) console.log(err);
-    res.redirect(`/rectangles/${id}`);
-  });
+  if (!isNaN(id)) {
+    const { name, color, width, height } = req.body;
+    const poolQuery = `UPDATE rectangle SET (name, color, width, height) = ('${name}', '${color}', '${width}','${height}') WHERE id='${id}'`;
+  
+    pool.query(poolQuery, (err, result) => {
+      if (err) console.log(err);
+      else res.redirect(`/rectangles/${id}`);
+    });
+  } else {
+    res.render('pages/error');
+  }
 });
 
 app.get('/rectangles/:id/delete', (req, res) => {
-  console.log('[GET]:', '/rectangles/:id/delete');
-
   const id = req.params.id;
 
-  const poolQuery = `DELETE FROM rectangle WHERE id='${id}'`;
+  if (!isNaN(id)) {
+    const poolQuery = `DELETE FROM rectangle WHERE id='${id}'`;
 
-  pool.query(poolQuery, (err, result) => {
-    if (err) console.log(err);
-    res.redirect('/rectangles');
-  });
+    pool.query(poolQuery, (err, result) => {
+      if (err) console.log(err);
+      else res.redirect('/rectangles');
+    });
+  } else {
+    res.render('pages/error');
+  }
+});
+
+app.get('*', (req, res) => {
+  res.render('pages/error');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 /* 
-  need to do error handling
-
-    - what happens when the user goes to a page that does not exist?
-
-    - what happens when the user passes an id into the url that does not exist?
-
-    - what happens when the user submits add rectangle with no/partial inputs?
-
-    - what happens when the user submits update rectangle with no/partial inputs?
+  - what happens when the user submits add rectangle with no/partial inputs?
+  - what happens when the user submits update rectangle with no/partial inputs?
 
   need to do layout and styling
+  need to add additional creative attributes to the table that the user can edit
+
+  look into SSL stuff with Heroku and Postgres
+
+  question: can the UI look anyway we want? does it need to follow the look that is on canvas?
 */
